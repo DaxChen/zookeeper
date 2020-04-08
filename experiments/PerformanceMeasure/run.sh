3a1a28aad374e3036553ffa75222f7aa1621b90c
DaxChen <dd@daxchen.com>
2020-04-07 19:27:18 -0500

    makefile and run.sh

---
+++ experiments/PerformanceMeasure/run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+NC='\033[0m' # No color
+
+# ask user what to run
+PS3='What do you want to run?'
+options=(
+  "MeasureSetDataStrongWeak"
+  "MeasureSetDataAsync"
+  "Quit"
+)
+select opt in "${options[@]}"
+do
+  case $opt in
+    'MeasureSetDataStrongWeak'| \
+    'MeasureSetDataAsync')
+      java -cp "./bin:../../build/zookeeper-3.4.12.jar:../../build/lib/*" performancemeasure.$opt
+      break
+      ;;
+    'Quit')
+      echo -e "\nBye~~~~~~"
+      break
+      ;;
+    *) echo "Invalid option.. $REPLY try again";;
+  esac
+done
+
